eac712378e0943e7ed061d631b20f2dcb354f9c7
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2025-07-20 17:25:20 +0530

    Time: 4 ms (65.08%) | Memory: 44.3 MB (57.52%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,15 +1,12 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        StringBuilder str = new StringBuilder(s);
+        StringBuilder str = new StringBuilder();
         int n = part.length();
-        int i=0;
-        while(i<str.length()){
-            if(i+n <= str.length() && str.substring(i, i+n).equals(part)){
-                str.delete(i, i+n);
-                i=0;
-            }
-            else{
-                i++;
+        for(char ch : s.toCharArray()){
+            str.append(ch);
+
+            if(str.length() >= n && str.substring(str.length()-n).equals(part)){
+                str.delete(str.length()-n, str.length());
             }
         }
         return str.toString();
