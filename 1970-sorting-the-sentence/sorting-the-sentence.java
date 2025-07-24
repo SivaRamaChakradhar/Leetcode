c11a7c9d0ef8fca86ec3884774692de445b59ecc
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2025-07-24 18:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (58.71%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -1,18 +1,11 @@
 class Solution {
     public String sortSentence(String s) {
-        TreeMap<Integer, String> ans = new TreeMap<>();
         String[] str = s.split(" ");
+        String[] res = new String[str.length];
         for(String st : str){
-            char n = st.charAt(st.length()-1);
-            int num = n-'0';
-            ans.put(num, st.substring(0, st.length()-1));
+            int n = st.charAt(st.length()-1)-'0';
+            res[n-1] = st.substring(0, st.length()-1);
         }
-        StringBuilder sb = new StringBuilder();
-        for(int key: ans.keySet()){
-            sb.append(ans.get(key));
-            sb.append(" ");
-        }
-        sb.deleteCharAt(sb.length()-1);
-        return sb.toString();
+        return String.join(" ", res);
     }
 }
